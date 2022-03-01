563e2f53fecba0eeeaf416ffac8a17a4a12bf902
JINYOUNGCHOI <cjy9249@tmon.co.kr>
2022-03-01 17:51:26 +0900

    feat : 백준 1062 가르침

---
+++ src/main/java/GS_Retail/GS_Retail_3.java
@@ -0,0 +1,15 @@
+package GS_Retail;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class GS_Retail_3 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+}
